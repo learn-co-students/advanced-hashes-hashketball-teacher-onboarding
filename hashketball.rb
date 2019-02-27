2a2c35d54baaac3431506ee3e03c32f17facad33
essential-gemfile-5901 <kyle.coberly@gmail.com>
2019-02-27 23:50:18 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,182 @@
-# Write your code here!
+require "pry"
 
+def game_hash
+  {
+    home: {
+      team_name: 'Brooklyn Nets',
+      colors: ['Black', 'White'],
+      players: {
+        'Alan Anderson' => {
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        },
+        'Reggie Evans' => {
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        },
+        'Brook Lopez' => {
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        },
+        'Mason Plumlee' => {
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        },
+        'Jason Terry' => {
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      }
+    },
+    away: {
+      team_name: 'Charlotte Hornets',
+      colors: ['Turquoise', 'Purple'],
+      players: {
+        'Jeff Adrien' => {
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        },
+        'Bismak Biyombo' => {
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        },
+        'DeSagna Diop' => {
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        },
+        'Ben Gordon' => {
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        },
+        'Brendan Haywood' => {
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        },
+      }
+    }
+  }
+end
 
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+      team_data.each do |attribute, data|
+        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+        binding.pry
 
+        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+        data.each do |data_item|
+            binding.pry
+      end
+    end
+  end
+end
 
+def players
+  game_hash[:away][:players].merge(game_hash[:home][:players])
+end
 
+def num_points_scored player_name
+  players[player_name][:points]
+end
 
+def shoe_size player_name
+  players[player_name][:shoe]
+end
 
+def get_team team_name
+  if game_hash[:home][:team_name].eql? team_name
+    return game_hash[:home]
+  elsif game_hash[:away][:team_name].eql? team_name
+    return game_hash[:away]
+  end
+end
 
+def team_colors team_name
+  get_team(team_name)[:colors]
+end
 
+def team_names
+  game_hash.map { |_, team|
+    team[:team_name]
+  }
+end
 
+def player_numbers team_name
+  get_team(team_name)[:players].map { |_, stats| stats[:number] }
+end
+
+def player_stats player_name
+  players[player_name]
+end
+
+def biggest_shoe
+  players.reduce({}) { |max, (_, current)|
+    (!max[:shoe] || current[:shoe] > max[:shoe]) ? current : max
+  }
+end
+
+def big_shoe_rebounds
+  biggest_shoe[:rebounds]
+end
